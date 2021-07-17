9df5acf8b4e0e37597fd26f0f2b263fcbb0b2895
M. Kamil <mk.kamiil@gmail.com>
2021-07-17 17:47:04 +0700

    Register layout

---
+++ app/src/main/java/com/mkamilmistar/gold_market/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.mkamilmistar.gold_market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_register)
+  }
+}

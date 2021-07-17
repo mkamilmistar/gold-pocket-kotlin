465ad1c6e4dc94abfec74c5c3bed7c0a4a4c5a09
M. Kamil <mk.kamiil@gmail.com>
2021-07-17 10:58:12 +0700

    move splashscreen to new activity

--- app/src/main/java/com/mkamilmistar/gold_market/MainActivity.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.mkamilmistar.gold_market
 
-import android.os.Build
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import android.widget.TextView
+import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    @Suppress("DEPRECATION")
-    window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-      WindowManager.LayoutParams.FLAG_FULLSCREEN);
     setContentView(R.layout.activity_main)
 
-    val topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-    val botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-
-    splash_logo.startAnimation(topAnim)
-    logo_text.startAnimation(botAnim)
-    slogan_text.startAnimation(botAnim)
   }
 }
---
+++ app/src/main/java/com/mkamilmistar/gold_market/SplashScreenActivity.kt
@@ -0,0 +1,35 @@
+package com.mkamilmistar.gold_market
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    @Suppress("DEPRECATION")
+    window.setFlags(
+      WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    setContentView(R.layout.activity_splash_screen)
+
+    val topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+    val botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+    splash_logo.startAnimation(topAnim)
+    logo_text.startAnimation(botAnim)
+    slogan_text.startAnimation(botAnim)
+
+    Handler(Looper.getMainLooper()).postDelayed({
+      val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+      startActivity(intent)
+      finish()
+    }, 3000)
+  }
+}

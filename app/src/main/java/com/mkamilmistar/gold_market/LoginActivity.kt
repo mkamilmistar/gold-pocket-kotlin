0fecd710c006e545b43cb42af6b802fa091dd06c
M. Kamil <mk.kamiil@gmail.com>
2021-07-17 21:45:29 +0700

    OnBoarding layout

--- app/src/main/java/com/mkamilmistar/gold_market/LoginActivity.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.mkamilmistar.gold_market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class LoginActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    @Suppress("DEPRECATION")
+    window.setFlags(
+      WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    setContentView(R.layout.activity_splash_screen)
     setContentView(R.layout.activity_login)
   }
 }

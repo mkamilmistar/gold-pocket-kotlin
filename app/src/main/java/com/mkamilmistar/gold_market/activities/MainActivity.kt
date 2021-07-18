5ebd2ad392d74b67810c6a5490d700329d5d6642
M. Kamil <mk.kamiil@gmail.com>
2021-07-18 21:37:29 +0700

    Bottom navigation bar

--- app/src/main/java/com/mkamilmistar/gold_market/activities/MainActivity.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/activities/MainActivity.kt
@@ -2,13 +2,40 @@ package com.mkamilmistar.gold_market.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mkamilmistar.gold_market.R
+import com.mkamilmistar.gold_market.fragments.bottomNavigation.HistoryFragment
+import com.mkamilmistar.gold_market.fragments.bottomNavigation.HomeFragment
+import com.mkamilmistar.gold_market.fragments.bottomNavigation.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+  private val homeFragment = HomeFragment()
+  private val historyFragment = HistoryFragment()
+  private val profileFragment = ProfileFragment()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    replaceFragment(homeFragment)
 
+    val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+    bottomNav.setOnItemSelectedListener{
+      when(it.itemId){
+        R.id.ic_home -> {
+          replaceFragment(homeFragment)
+        }
+        R.id.ic_history -> replaceFragment(historyFragment)
+        R.id.ic_profile -> replaceFragment(profileFragment)
+      }
+      true
+    }
+  }
+
+  private fun replaceFragment(fragment: Fragment) {
+    val transaction = supportFragmentManager.beginTransaction()
+    transaction.replace(R.id.fragment_container, fragment)
+    transaction.commit()
   }
 }
---
+++ app/src/main/java/com/mkamilmistar/gold_market/fragments/bottomNavigation/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.mkamilmistar.gold_market.fragments.bottomNavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mkamilmistar.gold_market.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}

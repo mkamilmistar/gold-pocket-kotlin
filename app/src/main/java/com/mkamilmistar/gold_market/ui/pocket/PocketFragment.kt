f41b45c5b730ac371a579190efab4edd2140b794
M. Kamil <mk.kamiil@gmail.com>
2021-08-01 21:02:47 +0700

    pocket

--- app/src/main/java/com/mkamilmistar/gold_market/ui/pocket/PocketFragment.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/ui/pocket/PocketFragment.kt
@@ -1,21 +1,38 @@
 package com.mkamilmistar.gold_market.ui.pocket
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mkamilmistar.gold_market.R
+import com.mkamilmistar.gold_market.data.model.Pocket
+import com.mkamilmistar.gold_market.data.model.Purchase
 import com.mkamilmistar.gold_market.databinding.FragmentPocketBinding
 import com.mkamilmistar.gold_market.databinding.FragmentRegisterBinding
+import com.mkamilmistar.gold_market.di.DependencyContainer
+import com.mkamilmistar.gold_market.helpers.EventResult
+import com.mkamilmistar.gold_market.ui.history.HistoryAdapter
+import com.mkamilmistar.gold_market.ui.history.HistoryViewModel
 
-class PocketFragment : Fragment() {
+class PocketFragment : Fragment(), PocketAdapter.OnClickItemListener {
 
   private lateinit var binding: FragmentPocketBinding
+  private lateinit var pocketAdapter: PocketAdapter
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  private val factory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+      return DependencyContainer().pocketViewModel as T
+    }
   }
+  private val viewModel: PocketViewModel by viewModels { factory }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +42,46 @@ class PocketFragment : Fragment() {
     return binding.root
   }
 
-  companion object {
-    @JvmStatic
-    fun newInstance() = PocketFragment()
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    subscribe()
+    viewModel.start()
+    binding.apply {
+      recycleViewPocket.apply {
+        layoutManager = LinearLayoutManager(context)
+        adapter = pocketAdapter
+      }
+    }
+  }
+
+  private fun subscribe() {
+    binding.apply {
+      pocketAdapter = PocketAdapter(this@PocketFragment)
+      val historyObserver: Observer<EventResult> = Observer<EventResult> { event ->
+        when (event) {
+          is EventResult.Loading -> Log.d("PocketFragment", "Loading...")
+          is EventResult.Success -> {
+            Log.d("PocketFragment", "Success...")
+            pocketAdapter.updateData(event.data as List<Pocket>)
+          }
+          is EventResult.Failed -> Log.d("HistoryFragment", "FAILED")
+          else -> {
+          }
+        }
+      }
+      viewModel.pocketLiveData.observe(viewLifecycleOwner, historyObserver)
+    }
+  }
+
+  override fun onClickItem(position: Int) {
+    Toast.makeText(context, viewModel.getPocket(position).id, Toast.LENGTH_SHORT).show()
+  }
+
+  override fun deleteItem(position: Int) {
+    Toast.makeText(context, viewModel.getPocket(position).id, Toast.LENGTH_SHORT).show()
+  }
+
+  override fun editItem(position: Int) {
+    Toast.makeText(context, viewModel.getPocket(position).id, Toast.LENGTH_SHORT).show()
   }
 }
--- app/src/main/java/com/mkamilmistar/gold_market/ui/pocket/PocketViewModel.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/ui/pocket/PocketViewModel.kt
@@ -1,7 +1,41 @@
 package com.mkamilmistar.gold_market.ui.pocket
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mkamilmistar.gold_market.data.model.Pocket
+import com.mkamilmistar.gold_market.data.model.Purchase
 import com.mkamilmistar.gold_market.data.repository.PocketRepository
+import com.mkamilmistar.gold_market.helpers.EventResult
+import java.lang.Exception
 
 class PocketViewModel(private val repository: PocketRepository): ViewModel() {
+  private var _pocketLiveData = MutableLiveData<EventResult>(EventResult.Idle)
+  val pocketLiveData: LiveData<EventResult>
+    get() = _pocketLiveData
+
+  fun start() {
+    updateData()
+  }
+
+  private fun getPocketRepository() = repository.findAllPocket()
+
+  private fun updateData() {
+    _pocketLiveData.value = EventResult.Loading
+    try {
+      val history: List<Pocket> = getPocketRepository()
+      _pocketLiveData.value = EventResult.Success(history)
+    } catch (e: Exception) {
+      _pocketLiveData.value = EventResult.Failed("Oops something wrong")
+    }
+  }
+
+  fun getPocket(position: Int): Pocket {
+    return repository.findPocket(position)
+  }
+
+  fun deletePocket(position: Int) {
+    repository.deletePocket(position)
+  }
+
 }

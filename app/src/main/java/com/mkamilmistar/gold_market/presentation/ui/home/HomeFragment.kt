9ccaaa93e2bb7a71c3f1a7b4083bd89d86be50e7
M. Kamil <mk.kamiil@gmail.com>
2021-08-15 11:20:56 +0700

    tes

--- app/src/main/java/com/mkamilmistar/gold_market/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/presentation/ui/home/HomeFragment.kt
@@ -3,11 +3,14 @@ package com.mkamilmistar.gold_market.presentation.ui.home
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.res.Configuration
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -46,14 +49,16 @@ class HomeFragment : Fragment() {
   private lateinit var profileViewModels: ProfileViewModel
   private lateinit var pocketViewModels: PocketViewModel
   private lateinit var activateCustomer: String
+  private lateinit var purchaseBuy: Purchase
+  private lateinit var purchaseSell: Purchase
 
 
   var product: Product = Product(
     productId = 1, productName = "TOLOL", productImage = "TEMPE", productPriceBuy = 100000, productPriceSell = 120000,
     productStatus = 1, updatedDate = "12 Maret 2021", createdDate = "10 Maret 2021"
   )
-  private var purchaseBuy: Purchase = Purchase(1, formatDate(LocalDateTime.now().toString()), 0, 100000, 1.0, 1)
-  private var purchaseSell: Purchase = Purchase(2, formatDate(LocalDateTime.now().toString()), 1, 120000, 1.0, 1)
+//  private var purchaseBuy: Purchase = Purchase(1, formatDate(LocalDateTime.now().toString()), 0, 100000, 1.0, 1)
+//  private var purchaseSell: Purchase = Purchase(2, formatDate(LocalDateTime.now().toString()), 1, 120000, 1.0, 1)
 
 
   override fun onCreateView(
@@ -94,6 +99,7 @@ class HomeFragment : Fragment() {
     profileViewModels.getCustomerById(activateCustomer.toInt())
     subscribe()
     binding.apply {
+
     }
   }
 
@@ -108,11 +114,11 @@ class HomeFragment : Fragment() {
   }
 
   fun buyProductHandle() {
-    showDialog("Are sure want to buy this product?", "Click OK to Continue", purchaseBuy)
+    purchaseDialog("Are sure want to BUY this product?", "Click OK to Continue", 0)
   }
 
   fun sellProductHandle() {
-    showDialog("Are sure want to sell this product?", "Click OK to Continue", purchaseSell)
+    purchaseDialog("Are sure want to SELL this product?", "Click OK to Continue", 1)
   }
 
   @SuppressLint("SetTextI18n")
@@ -206,12 +212,37 @@ class HomeFragment : Fragment() {
           }
         }
       }
-
       productViewModels.productLiveData.observe(viewLifecycleOwner, productObserver)
       profileViewModels.customerLivedata.observe(viewLifecycleOwner, profileObserver)
       pocketViewModels.pocketLiveData.observe(viewLifecycleOwner, pocketObserver)
       purchaseViewModels.isSuccess.observe(viewLifecycleOwner, purchaseObserver)
     }
+  }
+
+  private fun purchaseDialog(title: String, message: String, purchaseType: Int) {
+    val inputQty = EditText(requireActivity())
+    inputQty.inputType = InputType.TYPE_CLASS_NUMBER
+    inputQty.setRawInputType(Configuration.KEYBOARD_12KEY)
+    val dialog = AlertDialog.Builder(requireContext())
+      .setTitle("Create New Purchase")
+      .setMessage("Input Quantity")
+      .setView(inputQty)
+      .setPositiveButton("Purchase") { _, _ ->
+        val qty: Double = inputQty.text.toString().toDouble()
+        val price = if (purchaseType == 0) {
+          product.productPriceBuy * qty
+        } else {
+          product.productPriceSell * qty
+        }
+        purchaseBuy = Purchase(
+          purchaseDate = formatDate(LocalDateTime.now().toString()),
+          purchaseType = purchaseType, price = price.toInt(), qtyInGram = qty,
+          customerPurchaseId = activateCustomer.toInt())
+        showDialog(title, message, purchaseBuy)
+      }
+      .setNegativeButton("Cancel", null)
+      .create()
+    dialog.show()
   }
 
   private fun showDialog(title: String, message: String, purchase: Purchase) {

6063d5218ab4dd55a9ebfbf73fd66c77a6970a05
M. Kamil <mk.kamiil@gmail.com>
2021-08-15 21:55:34 +0700

    fix

--- app/src/main/java/com/mkamilmistar/gold_market/presentation/ui/pocket/PocketFragment.kt
+++ app/src/main/java/com/mkamilmistar/gold_market/presentation/ui/pocket/PocketFragment.kt
@@ -3,6 +3,7 @@ package com.mkamilmistar.gold_market.presentation.ui.pocket
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mkamilmistar.gold_market.R
 import com.mkamilmistar.gold_market.data.db.AppDatabase
@@ -109,10 +112,18 @@ class PocketFragment : Fragment(), PocketAdapter.OnClickItemListener {
     }
   }
 
+  private fun navToHome() {
+    findNavController().navigate(
+      R.id.homeFragment, null,
+      NavOptions.Builder().setPopUpTo(R.id.nav_graph, true).build()
+    )
+  }
+
   override fun onClickItem(position: Int) {
     val sharedPref = SharedPref(requireContext())
     val selectedPocket = pocketViewModel.getPocketById(position)
     sharedPref.save(Utils.POCKET_ID, selectedPocket.pocketId.toString())
+    navToHome()
     Toast.makeText(context, "${selectedPocket.pocketName} Selected", Toast.LENGTH_SHORT).show()
   }
 
@@ -150,5 +161,10 @@ class PocketFragment : Fragment(), PocketAdapter.OnClickItemListener {
 
   private fun showProgressBar() {
     binding.progressBarPocket.visibility = View.VISIBLE
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.d("PocketFragment", "DESTROYED")
   }
 }
